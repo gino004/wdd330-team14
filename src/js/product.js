86294873bc5bc0ba471276a9c4510e19e2cebc2a
gino004 <gino2137@hotmail.com>
2025-05-15 18:53:32 -0500

    Fix

--- src/js/product.js
+++ src/js/product.js
@@ -4,17 +4,17 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
+	const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
+	cartItems.push(product);
+	setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
+	const product = await dataSource.findProductById(e.target.dataset.id);
+	addProductToCart(product);
 }
 
 // add listener to Add to Cart button
 document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+	.getElementById("addToCart")
+	.addEventListener("click", addToCartHandler);
