a9cd5f532e7927b48322fa98ef15154706fb8167
VictorV <vvega28@outlook.com>
2025-05-27 01:19:37 -0600

    Total$ in cart added

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,6 +4,21 @@ function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+  //TOTAL $ IN CART
+  if (cartItems.length > 0) {
+    // Show the cart-Total$
+    const totalPrice = document.querySelector(".cart-totalPrice");
+    totalPrice.classList.remove("hide");
+
+    // Calculate total
+    const total = cartItems
+      .reduce((sum, item) => sum + Number(item.FinalPrice), 0)
+      .toFixed(2);
+
+    // Display total
+    totalPrice.querySelector(".cart-total").textContent = `Total: $${total}`;
+  }
 }
 
 function cartItemTemplate(item) {
@@ -20,8 +35,10 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
-
+</li>
+<div>
+  <p class="cart-total">Total: </p>
+</div>`;
   return newItem;
 }
 
