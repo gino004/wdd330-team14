8645fbef9d7df58ad848f9e610cabc54627c17ce
Offizil <fredoffizil@gmail.com>
2025-05-28 23:54:34 +0100

    updated CTA

--- src/js/main.js
+++ src/js/main.js
@@ -3,22 +3,19 @@ import Alert from "./alert.mjs";
 const alertInstance = new Alert("/json/alerts.json");
 alertInstance.render();
 
-
-
-
 function showWelcomeModal() {
-    const modalShown = localStorage.getItem("welcome-modal-shown");
-  
-    if (!modalShown) {
-      const modal = document.getElementById("welcome-modal");
-      modal.classList.remove("hidden");
-  
-      document.querySelector(".close-button").addEventListener("click", () => {
-        modal.classList.add("hidden");
-        localStorage.setItem("welcome-modal-shown", "true");
-      });
-    }
+  const modalShown = localStorage.getItem("welcome-modal-shown");
+
+  if (!modalShown) {
+    const modal = document.getElementById("welcome-modal");
+    modal.classList.remove("hidden");
+
+    document.querySelector(".close-button").addEventListener("click", () => {
+      modal.classList.add("hidden");
+      localStorage.setItem("welcome-modal-shown", "true");
+    });
   }
-  
-  // Call this when the page loads
-  document.addEventListener("DOMContentLoaded", showWelcomeModal);
+}
+
+// Call this when the page loads
+document.addEventListener("DOMContentLoaded", showWelcomeModal);
